b3a6940d56e9f57e894cfb910bf6201e8181eb97
Audrey M Roy <audreyr@gmail.com>
2011-07-15 22:22:51 -0700

    Trying to solve settings import issue.

--- deploy/wsgi.py
+++ deploy/wsgi.py
@@ -1,6 +1,6 @@
 import os, sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir)))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir)))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
 
 from django.core.handlers.wsgi import WSGIHandler
