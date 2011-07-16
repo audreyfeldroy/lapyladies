00e35ac06683462a27ead74b3920e94f8abec8fa
Audrey M Roy <audreyr@gmail.com>
2011-07-15 22:19:28 -0700

    Moved calendar to /, added gondor, added footer.

--- urls.py
+++ urls.py
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^calendar/', direct_to_template, {'template': 'gcal/calendar.html'}),
+    url(r'^$', direct_to_template, {'template': 'gcal/calendar.html'}),
 )
